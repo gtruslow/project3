bccabedda7ec9179621098a386ddc4f614d30276
gtruslow <gary.truslow@gmail.com>
2017-02-09 14:58:10 -0500

    changed a little

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
 	public static void main( String [] args) {
 
-		System.out.println("Hello GIT in java for project3\n");
+		System.out.println("Hello GIT in java for project3 develop\n");
 	}
 }
